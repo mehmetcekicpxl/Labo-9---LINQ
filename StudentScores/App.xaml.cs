9c75483a1b9e589ca0631491130a2ba8a27bd7be
Wim Bervoets <wim.bervoets@pxl.be>
2025-05-11 19:47:34 +0200

    Add project files.

---
+++ StudentScores/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace StudentScores
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
